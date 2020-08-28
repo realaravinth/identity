78aefae0b1867ac5f89876e5439779dd28059b4c
realaravinth <realaravinth@batsense.net>
2020-08-28 19:52:55 +0530

    more error types

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,10 @@
 use actix_http::ResponseBuilder;
-use actix_web::{error, http::header, http::StatusCode, HttpResponse};
+use actix_web::{error::ResponseError, http::header, http::StatusCode, HttpResponse};
+use diesel::result::Error as DBError;
 use failure::Fail;
 use serde::{Deserialize, Serialize};
+
+use std::convert::From;
 
 #[derive(Debug, Fail)]
 pub enum ServiceError {
@@ -12,11 +15,11 @@ pub enum ServiceError {
     #[fail(display = "invalid credentials")]
     AuthorizationRequired,
     #[fail(display = "internal error")] // 500
-    InternalError,
+    InternalServerError,
     #[fail(display = "timeout")] //408
     Timeout,
     #[fail(display = "bad request")] //400
-    BadClientData,
+    BadRequest,
     #[fail(display = "Unable to connect to DB")]
     UnableToConnectToDb,
 }
@@ -26,7 +29,7 @@ struct ErrorToResponse {
     error: String,
 }
 
-impl error::ResponseError for ServiceError {
+impl ResponseError for ServiceError {
     fn error_response(&self) -> HttpResponse {
         ResponseBuilder::new(self.status_code())
             .set_header(header::CONTENT_TYPE, "application/json; charset=UTF-8")
@@ -40,10 +43,23 @@ impl error::ResponseError for ServiceError {
             ServiceError::CharError => StatusCode::METHOD_NOT_ALLOWED,
             ServiceError::UsernameExists => StatusCode::METHOD_NOT_ALLOWED,
             ServiceError::AuthorizationRequired => StatusCode::UNAUTHORIZED,
-            ServiceError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
-            ServiceError::BadClientData => StatusCode::BAD_REQUEST,
+            ServiceError::InternalServerError => StatusCode::INTERNAL_SERVER_ERROR,
+            ServiceError::BadRequest => StatusCode::BAD_REQUEST,
             ServiceError::Timeout => StatusCode::GATEWAY_TIMEOUT,
             ServiceError::UnableToConnectToDb => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
+
+impl From<DBError> for ServiceError {
+    fn from(error: DBError) -> ServiceError {
+        // Right now we just care about UniqueViolation from diesel
+        // But this would be helpful to easily map errors as our app grows
+        match error {
+            DBError::DatabaseError(_kind, _info) => ServiceError::BadRequest,
+            _ => ServiceError::InternalServerError,
+        }
+    }
+}
+
+pub type ServiceResult<V> = std::result::Result<V, crate::errors::ServiceError>;
