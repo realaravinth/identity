068823deb69eeac2af5b3b6dc7cc358eef1b7d5a
realaravinth <realaravinth@batsense.net>
2020-08-30 22:57:51 +0530

    filters deployed

--- src/users/utils/utils.rs
+++ src/users/utils/utils.rs
@@ -2,7 +2,10 @@ use super::hashify::create_hash;
 use crate::database::pool::ConnectionPool;
 use crate::errors::ServiceResult;
 use crate::schema::users;
+use crate::users::filters::blacklist::enforce::forbidden;
+use crate::users::filters::profainity::enforce::beep;
 use crate::users::filters::user_case_mapped::enforce::filter;
+
 use crate::users::models;
 pub async fn create_new_user(
     con: &ConnectionPool,
@@ -10,6 +13,10 @@ pub async fn create_new_user(
     password: &str,
 ) -> ServiceResult<()> {
     let normalised_username = filter(username)?;
+    beep(username)?;
+    beep(&normalised_username)?;
+    forbidden(username)?;
+    forbidden(&normalised_username)?;
     let hash = create_hash(password);
     todo!(
         "
